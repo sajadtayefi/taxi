c4f929a4e9144c8c93ca5960d1e3bd273ae21129
majidzaheid <eemajidzahedi@hotmail.com>
2022-05-22 17:44:25 +0430

    some new update

---
+++ js/custom.js
@@ -0,0 +1,74 @@
+const swiper1 = new Swiper('.swiper1', {
+    slidesPerView: 1.2,
+    initialSlide: 1,
+    longSwipes: false,
+    loop: true,
+    centeredSlides: true,
+    speed: 1000,
+    spaceBetween: -80,
+    effect: 'coverflow',
+    direction: 'horizontal',
+    navigation: {
+        nextEl: ".swiper-button-next1",
+        prevEl: ".swiper-button-prev1",
+    },
+    breakpoints: {
+        // when window width is >= 376px
+        376: {
+            slidesPerView: 1.5,
+            spaceBetween: -120
+        },
+        // when window width is >= 550px
+        550: {
+            slidesPerView: 1.8,
+            spaceBetween: -140
+        },
+        // when window width is >= 768px
+        768: {
+            slidesPerView: 2.1,
+            spaceBetween: -180
+        },
+        // when window width is >= 1278px
+        1278: {
+            slidesPerView: 3,
+            spaceBetween: -450
+        }
+    }
+});
+const swiper2 = new Swiper('.swiperNum2', {
+    slidesPerView: 1.2,
+    initialSlide: 1,
+    longSwipes: false,
+    loop: true,
+    centeredSlides: true,
+    speed: 1000,
+    spaceBetween: -80,
+    effect: 'coverflow',
+    direction: 'horizontal',
+    navigation: {
+        nextEl: ".swiper-button-next2",
+        prevEl: ".swiper-button-prev2",
+    },
+    breakpoints: {
+        // when window width is >= 376px
+        376: {
+            slidesPerView: 1.5,
+            spaceBetween: -120
+        },
+        // when window width is >= 550px
+        550: {
+            slidesPerView: 1.8,
+            spaceBetween: -140
+        },
+        // when window width is >= 768px
+        768: {
+            slidesPerView: 2.1,
+            spaceBetween: -180
+        },
+        // when window width is >= 1278px
+        1278: {
+            slidesPerView: 3,
+            spaceBetween: -450
+        }
+    }
+});
